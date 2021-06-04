b91262c5ca540a2be3e8b69058114fd2ba4b22ea
Sunpacker <sunpacker@yandex.ru>
2021-06-04 20:56:46 +0400

    добавлена delta

---
+++ js/common.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+	
+	$('.sl').slick({
+		arrows:false,
+		dots: true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	})
+	
+	$('.picture').fancybox()
+	
+})
